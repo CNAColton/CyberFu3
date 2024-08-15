efde0d038f43df5af582d83f75f46b1ddbec2ae4
CNAColton <CNAColton@gmail.com>
2024-08-15 18:57:15 -0400

    8-15

---
+++ CyberFu2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentPlayerHealth = 10;
+    public int enemyDamage = 5;
+
+    void Start()
+    {
+
+    }
+
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamage;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HitCollider"))
+        {
+            HurtPlayer();
+        }
+    }
+}
